99c969a0fefca986916146418a8360eae3a569d2
JoanRossi <elpibedelapc@gmail.com>
2022-10-18 18:06:42 -0300

    ACTUALIZACION PARA QUE ANDE LA BASE DE DATOS POR QUE NO ANDABA Y ME AYUDO UN AMIGO ASI QUE GOD

--- 05-spring-boot-mysql-jpa-simple-main/simple.sql
+++ 05-spring-boot-mysql-jpa-simple-main/simple.sql
@@ -1,28 +1,20 @@
-DROP DATABASE IF EXISTS `simple`;
+DROP DATABASE IF EXISTS simple;
 
-CREATE DATABASE IF NOT EXISTS `simple` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE DATABASE IF NOT EXISTS simple;
 
-DROP USER `springuser` @`%`;
+USE simple;
 
-CREATE USER `springuser` @`%` IDENTIFIED BY 'ThePassword';
+DROP TABLE IF EXISTS user;
 
-GRANT USAGE ON *.* TO `springuser` @`%`;
-
-GRANT ALL PRIVILEGES ON `simple`.* TO ' springuser ' @' % ';
-
-USE `simple`;
-
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE IF NOT EXISTS `user` (
-    `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-    `email` varchar(255) DEFAULT NULL,
-    `name` varchar(255) DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB AUTO_INCREMENT = 10 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+CREATE TABLE IF NOT EXISTS user (
+    id int UNSIGNED NOT NULL AUTO_INCREMENT,
+    email varchar(255) DEFAULT NULL,
+    name varchar(255) DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB AUTO_INCREMENT = 10;
 
 INSERT INTO
-    `user` (`id`, `email`, `name`)
+    user (id, email, name)
 VALUES
     (1, 'uno@gmail.com', 'Uno'),
     (2, 'dos@gmail.com', 'Dos'),
