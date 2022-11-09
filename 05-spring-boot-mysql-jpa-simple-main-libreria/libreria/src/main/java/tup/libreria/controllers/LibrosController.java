f5142fa903e1fa78a44a2fc7b617f5f4df4f0d6a
JoanRossi <elpibedelapc@gmail.com>
2022-11-08 22:49:34 -0300

    punto1 para //:

--- 05-spring-boot-mysql-jpa-simple-main-libreria/libreria/src/main/java/tup/libreria/controllers/LibrosController.java
+++ 05-spring-boot-mysql-jpa-simple-main-libreria/libreria/src/main/java/tup/libreria/controllers/LibrosController.java
@@ -288,11 +288,50 @@ public class LibrosController {
     
     String boton = """
       
-      <a href= http://localhost:8080/libros/descripciones target="_blank" class=button >VER DESCRIPCIONES</a>
+      
+      <div class="center"><a href="http://localhost:8080/libros/descripciones" class="button">Press Here</a></div>
       <style>
-        .button{
-          text-decoration:none;
-          color: green;
+      body {
+        background-color: #fafafa;
+        font-family: sans-serif;
+        font-weight: 300;
+      }
+      
+      .button {
+        background-color: #FF5722;
+        border-radius: 6px;
+        box-shadow: 2px 2px 1px rgba(0,0,0,0.4);
+        color: white;
+        display: block;
+        font-size: 1.5em;
+        font-weight: 400;
+        overflow: hidden;
+        padding: 2em 4em;
+        position: relative;
+        text-decoration: none;
+        transition: all 200ms ease-out;
+      }
+      
+      .button:hover {
+        box-shadow: 3px 3px 5px rgba(0,0,0,0.3);
+      }
+      
+      .center {
+        left: 50%;
+        position: absolute;
+        top: 50%;
+        transform: translate(-50%,-50%);
+      }
+      
+      .ripple {
+        background-color: rgba(255,255,255,0.3);
+        opacity: 1;
+        position: absolute;
+        -webkit-transform: scale(0);
+        -moz-transform: scale(0);
+        transform: scale(0);
+        transition: all 500ms ease-out;
+      }
         }
       </style>
  
@@ -300,45 +339,130 @@ public class LibrosController {
 
     String tabla = """
           <style>
-            #libro {"
-              font-family: Arial, Helvetica, sans-serif;
-              border-collapse: collapse;
-              width: 100%;
-              
-            }
-            body{
-              background-image: url("https://static8.depositphotos.com/1457895/952/v/450/depositphotos_9529877-stock-illustration-books-background.jpg");
-            }
-            #libros td, #libros th {
-              border: 3px solid #ddd;
-              padding: 13px;
-            }
-            #libros tr:nth-child(n){background-color: #F9FF6E;}
-            #libros tr:hover {background-color: #F66E25;}
-            #libros th {
-              padding-top: 20px;
-              padding-bottom: 20px;
+          @charset "UTF-8";
+          @import url(https://fonts.googleapis.com/css?family=Open+Sans:300,400,700);
+          
+          body {
+            font-family: 'Open Sans', sans-serif;
+            font-weight: 300;
+            line-height: 1.42em;
+            color:#A7A1AE;
+            background-color:#1F2739;
+          }
+          
+          h1 {
+            font-size:3em; 
+            font-weight: 300;
+            line-height:1em;
+            text-align: center;
+            color: #4DC3FA;
+          }
+          
+          h2 {
+            font-size:1em; 
+            font-weight: 300;
+            text-align: center;
+            display: block;
+            line-height:1em;
+            padding-bottom: 2em;
+            color: #FB667A;
+          }
+          
+          h2 a {
+            font-weight: 700;
+            text-transform: uppercase;
+            color: #FB667A;
+            text-decoration: none;
+          }
+          
+          .blue { color: #185875; }
+          .yellow { color: #FFF842; }
+          
+          .container th h1 {
+              font-weight: bold;
+              font-size: 1em;
+            text-align: left;
+            color: #185875;
+          }
+          
+          .container td {
+              font-weight: normal;
+              font-size: 1em;
+            -webkit-box-shadow: 0 2px 2px -2px #0E1119;
+               -moz-box-shadow: 0 2px 2px -2px #0E1119;
+                    box-shadow: 0 2px 2px -2px #0E1119;
+          }
+          
+          .container {
               text-align: left;
-              background-color: #F6DD25;
-              color: black;
-            }
-              .center {
-                margin-left: auto;
-                margin-right: auto;
-                
-              }
+              overflow: hidden;
+              width: 80%;
+              margin: 0 auto;
+            display: table;
+            padding: 0 0 8em 0;
+          }
+          
+          .container td, .container th {
+              padding-bottom: 2%;
+              padding-top: 2%;
+            padding-left:2%;  
+          }
+          
+          /* Background-color of the odd rows */
+          .container tr:nth-child(odd) {
+              background-color: #323C50;
+          }
+          
+          /* Background-color of the even rows */
+          .container tr:nth-child(even) {
+              background-color: #2C3446;
+          }
+          
+          .container th {
+              background-color: #1F2739;
+          }
+          
+          .container td:first-child { color: #FB667A; }
+          
+          .container tr:hover {
+             background-color: #464A52;
+          -webkit-box-shadow: 0 6px 6px -6px #0E1119;
+               -moz-box-shadow: 0 6px 6px -6px #0E1119;
+                    box-shadow: 0 6px 6px -6px #0E1119;
+          }
+          
+          .container td:hover {
+            background-color: #FFF842;
+            color: #403E10;
+            font-weight: bold;
+            
+            box-shadow: #7F7C21 -1px 1px, #7F7C21 -2px 2px, #7F7C21 -3px 3px, #7F7C21 -4px 4px, #7F7C21 -5px 5px, #7F7C21 -6px 6px;
+            transform: translate3d(6px, -6px, 0);
+            
+            transition-delay: 0s;
+              transition-duration: 0.4s;
+              transition-property: all;
+            transition-timing-function: line;
+          }
+          
+          @media (max-width: 800px) {
+          .container td:nth-child(4),
+          .container th:nth-child(4) { display: none; }
+          }
 
           </style>
-          
-          <table id ='libros'>
-            <tr>
-              <th>ID</th>
-              <th>Genero</th>
-              <th>Libro</th>
-              <th>Precio</th>
-              <th>Descripcion </th>
-            </tr> 
-            
+
+
+<table id ='libros'>
+<table class="container">
+  <thead>
+    <tr>
+      <th><h1>Sites</h1></th>
+      <th><h1>Views</h1></th>
+      <th><h1>Clicks</h1></th>
+      <th><h1>Price</h1></th>
+    </tr>
+        
         """; 
         
     for (Libros libros : iterable) {
@@ -348,13 +472,30 @@ public class LibrosController {
           + "<td>" + libros.getGenero() + "</td>"
           + "<td>" + libros.getLibro() + "</td>"
           + "<td>" + libros.getPrecio() + "</td>"
-          + "<td>" + boton + "</td>"
           + "</tr>";
           
     } 
-    
-     return tabla + "</table>";
+
+
+     return tabla + "</table>" + boton;
   }
+
+
+//REPOSICIONAR BOTON PARA QUE QUEDE AL LADO DE LA TABLA. AUMENTAR TAMANIO DE LETRA DE HEADER Y COLOR AZUL MAS CLARO TIPO CYAN;
+
+// DESPUES COLOR NARANJA ELEMENTOS DE TABLA Y BACKGROUND QUE LE MANDE A MOTURA.
+//ponerle color blanco a las letras del boton de descripciones
+// ponerle fondo de la montania
+// cambiar el color blanco de la descripcion por un azul bien clarito pero que quede a tono con el fondop.
+
+
+
+
+
+
+
+
+
 
   @GetMapping("/libros/descripciones")
   public String descripciones() {
