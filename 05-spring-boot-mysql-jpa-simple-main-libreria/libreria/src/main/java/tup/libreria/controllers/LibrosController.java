9d3ad0403718d73c045b556cbc1c8901849086c5
JoanRossi <elpibedelapc@gmail.com>
2022-11-14 01:10:42 -0300

    EMPANADA

--- 05-spring-boot-mysql-jpa-simple-main-libreria/libreria/src/main/java/tup/libreria/controllers/LibrosController.java
+++ 05-spring-boot-mysql-jpa-simple-main-libreria/libreria/src/main/java/tup/libreria/controllers/LibrosController.java
@@ -99,6 +99,42 @@ public class LibrosController {
     return userRepository.findById(id).toString();
   }
 
+  @GetMapping("e")
+  public String f(){
+    String h="";
+    
+    String a = """
+      <div class="container">
+  
+      <h2>Google Material Design in CSS3<small>Inputs</small></h2>
+      
+      <form>
+        
+        <div class="group">      
+          <input type="text" required>
+          <span class="highlight"></span>
+          <span class="bar"></span>
+          <label>Name</label>
+        </div>
+          
+        <div class="group">      
+          <input type="text" required>
+          <span class="highlight"></span>
+          <span class="bar"></span>
+          <label>Email</label>
+        </div>
+        
+      </form>
+          
+      <p class="footer">
+        a <a href="https://scotch.io/tutorials/css/google-material-design-input-boxes-in-css3" target="_blank">tutorial</a> by <a href="https://scotch.io" target="_blank">scotch.io</a>
+      </p>
+      
+    </div>
+        """;
+        return a;
+  }
+
   @GetMapping("") 
   public String main() {
       // CAMBIAR LO QUE VIENE SIENDO EL COLOR DE LIBRERIA 9 LIBROS POR UN AMARILLO LINDO
@@ -457,7 +493,7 @@ public class LibrosController {
       <th><h1>Libro</h1></th>
       <th><h1>Precio</h1></th>
     </tr>
-        
+        	
         """; 
         
     for (Libros libros : iterable) {
